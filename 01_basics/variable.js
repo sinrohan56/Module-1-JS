aece21dfc4ff0281bfeab21b8be910f273f192c7
sinrohan56 <rohansingh12345698@gmail.com>
2024-09-08 12:27:03 +0000

    variables

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,5 +1,17 @@
-const a = "hitesh" //This const keyword is used whe you dont want to change your varible in code anywhere.
+const a = "hitesh" //This const keyword doesnt let the variable to be reassigned. Ex- id, 
 
-let a = "rohan"
+
+let b = "rohan" //This keyword lets the variable to be reassigned but outside their scope. You cannot change the value of the variable in the same scope.
+
+c = "ro"
+
+let i; // this returns undefined as ithing is defiend in the variable.
+
+
+
+ 
+
+
+
 
 

75d74fbeeeba564181eb5614f046cb46d35c8575
sinrohan56 <rohansingh12345698@gmail.com>
2024-09-08 12:43:52 +0000

    data types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // this enables the engine to treat all JS code as the newer version.
+
+console.log(a)  console.log(b)  //This will give an error because there is no distinguisher between two actions. we can give a semicolon to distinguish it. |
+console.log(a);  console.log(b)
+
+// dataTypes
+
+let name = "string" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+
+//bigInt - //number range > 2 to power -> isse bada number hai to bigInt use karna hota hai
+
+//null - its a null type with stadalone value.(representation of empty value, likes its khali) // 
+
+//undefined - when value is not defined.
+//symbol - unique data 
+
+//object 
+
+console.log(typeof "rohan")// Return the type of any data you provide
+console.log(typeof null) //it returns object - standard given type to null or it can also be said that it's JS's flaw.

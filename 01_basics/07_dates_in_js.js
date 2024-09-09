bdd2697e4fb23d6a35563009cea3cc49933d0e5a
sinrohan56 <rohansingh12345698@gmail.com>
2024-09-09 17:56:56 +0000

    date time v2

--- 01_basics/07_dates_in_js.js
+++ 01_basics/07_dates_in_js.js
@@ -7,8 +7,29 @@ let myDate = new Date()
 // console.log(myDate.toString());
 
 // console.log(myDate.toLocaleString('en-IN'));
+
 //if we want to declare any specific date.
 
 const dateCreated = new Date(2023, 0, 23)
 
 console.log(dateCreated);
+
+// to get a specific date format
+
+let dateFormat = new Date("01-14-2023") 
+
+//timestamps
+
+let myTimeStamps = Date.now()
+console.log(myTimeStamps); // returns millisecond value.
+
+//to get readable date from upper statement.
+//how to convert in seconds.
+console.log(Math.floor(Date.now()/1000)); // gives current time in second.
+
+//the locale string method can also defined in more details.
+
+myDate.toLocaleString('default', {
+    weekday: "long",
+})
+console.log(myDate)

031a3db253ee6d42d00bda53258211e312686751
sinrohan56 <rohansingh12345698@gmail.com>
2024-09-09 15:40:05 +0000

    datesv1

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,14 @@
+// the date is compared from a fixed date by organization if.e, jan 1 1970. And the value is returned in milliseconds.
+
+//Date is an object.
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toLocaleString('en-IN'));
+//if we want to declare any specific date.
+
+const dateCreated = new Date(2023, 0, 23)
+
+console.log(dateCreated);

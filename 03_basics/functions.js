d245be8a7bfefc64778ef444ed6b135d786ad261
sinrohan56 <rohansingh12345698@gmail.com>
2024-09-29 18:02:47 +0000

    functions2

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -37,3 +37,35 @@ const user = loginUserMessage("Rohan") //This will not give anything, as we didn
 console.log(user)
 
 //we ca also predefine some thing to the parameter like this so that we do not get undefined when user dont give any value in arguments. - function get(username = 'sam')
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+//we can use ...num1, spread operator to spread the given arguments store it in num1, but the first two numbers will go in val1 and val 2.
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
